a13e7209e759ddb147bd3a15b186746ca511c7e9
yzrlvu <cindymontoyagranda123@gmail.com>
2026-04-23 07:59:40 -0500

    fix: correct typo in response handling

--- src/api/index.js
+++ src/api/index.js
@@ -7,7 +7,7 @@ const server = http
 .createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/plain");
-    responde.end("Hola mundo! Un saludo");
+    response.end("Hola mundo! Un saludo");
 });
 
 server.listen(port, hostname, () => {
